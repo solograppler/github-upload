f429618a07ca24ef2359ca3ae63280d12798fc1a
Joel Nunez <joloves@gmail.com>
2021-08-15 15:54:55 -0400

    made some changes

--- flying_scroll.py
+++ flying_scroll.py
@@ -23,3 +23,37 @@ SCREEN_HEIGHT = 600
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Variable to keep the main loop running
+running = True
+
+#Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
+
+    # Fill the screen with white
+    screen.fill((255, 255, 255))
+
+    # Create a surface and pass a tuple containing its length and width
+    surf = pygame.Surface((50, 50))
+
+    # Give the surface a color to separate it from the background
+    surf.fill((0, 0, 0))
+    rect = surf.get_rect()
+
+    # This line says "Draw surf onto the screen at the center"
+    screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    pygame.display.flip()
+
+
+
